64afc14045098d9692054715206dbc1b710a8058
huezxiii <huesli.olibyer.0134@gmail.com>
2023-01-20 01:42:35 +0800

    added ribbon

--- js/app.js
+++ js/app.js
@@ -20,3 +20,39 @@ setInterval(function () {
   document.querySelector(".synonyms-section").style.backgroundColor = colors[i];
   i = (i + 1) % colors.length;
 }, 1500);
+
+
+// wrap the ribbon element in a javascript variable
+const ribbon = document.querySelector('.ribbon');
+// get the width of the entire ribbon
+const ribbonWidth = ribbon.offsetWidth;
+// get the width of one icon container
+const iconContainerWidth = document.querySelector('.icon-container').offsetWidth;
+// calculate the number of icon container elements in the ribbon
+const iconCount = Math.ceil(ribbonWidth / iconContainerWidth);
+// clone the first x number of icon container elements and add them to the end of the ribbon
+for (let i = 0; i < iconCount; i++) {
+  const iconClone = document.querySelector('.icon-container').cloneNode(true);
+  ribbon.appendChild(iconClone);
+}
+
+
+      // Get the button
+      var backToTopButton = document.getElementById("back-to-top-button");
+  
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function() {scrollFunction()};
+  
+      function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          backToTopButton.style.display = "block";
+        } else {
+          backToTopButton.style.display = "none";
+        }
+      }
+  
+      // When the user clicks on the button, scroll to the top of the document
+      function scrollToTop() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      }
